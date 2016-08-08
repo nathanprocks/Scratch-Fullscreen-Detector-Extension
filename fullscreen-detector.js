745f32a2437645feaf940c712a6b80832adde56e
Nathan Piercy <nathanprocks@hotmail.com>
2016-08-08 22:09:29 +1000

    Created extension script

---
+++ fullscreen-detector.js
@@ -0,0 +1,29 @@
+/*
+	Scratch Fullscreen Detector
+	Created by: Nathan Piercy (nathanprocks)
+*/
+
+(function(ext) {
+	// Cleanup function when the extension is unloaded
+	ext._shutdown = function() {};
+
+	// Status reporting code
+	// Use this to report missing hardware, plugin or unsupported browser
+	ext._getStatus = function() {
+		return {status: 2, msg: 'Ready'};
+	};
+
+	ext.isFullscreen = function() {
+		return false;
+	};
+
+	// Block and block menu descriptions
+	var descriptor = {
+		blocks: [
+			['b', 'is fullscreen?', 'isFullscreen']
+		]
+	};
+
+	// Register the extension
+	ScratchExtensions.register('Fullscreen Detector', descriptor, ext);
+})({});
